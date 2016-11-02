e8c0b15cb4702c38d8c8c1515279320345746532
Dylan Wulf <wulfd1@tcnj.edu>
2016-11-02 19:05:28 -0400

    Added get-terminals function

--- parse-table.rkt
+++ parse-table.rkt
@@ -1,4 +1,3 @@
-
 (define calc-gram
 `(("P" ("SL" "$$"))
 ("SL" ("S" "SL") ())
@@ -20,9 +19,57 @@
 
 (define (all x) (cons '(1) (list x)))
 
-(define (nonterms gram) (append (map car gram)))
+(define (nonterms gram) (map car gram))
 
 (define (contains list x)
     (cond ((null? list) #f)
         ((eq? (car list) x) #t)
         (else (contains (cdr list) x))))
+
+(define get-terminals
+  (lambda (gram)
+    (get-terminals-helper gram (nonterms gram))
+  )
+)
+
+(define get-terminals-helper
+  (lambda (gram nonterms)
+    (if (> (length gram) 0)
+        (addifne (get-terminals-helper (cdr gram) nonterms) (get-terms-rhs-list (cdr (car gram)) nonterms))
+        '()
+    )
+  )
+)
+
+(define get-terms-rhs-list
+  (lambda (rhs-list nonterms)
+    (if (> (length rhs-list) 0)
+        (addifne (get-terms-rhs-list (cdr rhs-list) nonterms) (get-terms-rhs (car rhs-list) nonterms))
+        '()
+    )
+  )
+)
+
+(define get-terms-rhs
+  (lambda (rhs nonterms)
+    (if (> (length rhs) 0)
+        (if (contains nonterms (car rhs))
+            (get-terms-rhs (cdr rhs) nonterms)
+            (addifne (get-terms-rhs (cdr rhs) nonterms) (list (car rhs)))
+        )
+        '()
+    )
+  )
+)
+
+(define addifne
+  (lambda (l x)
+    (if (> (length x) 1)
+        (addifne (addifne l (cdr x)) (list (car x)))
+        (if (or (eq? (length x) 0) (contains l (car x)))
+            l
+            (append l x)
+        )
+    )
+  )
+)
